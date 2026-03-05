c2db6e767a1089f9a572cf8be6d9c83d53f43ee2
joaocaeatano1310 <joaovictorcaetano1310@gmail.com>
2026-03-05 15:41:06 -0300

    Feat: "Adicionando método update com tratamento de erro na classe Curso.cs"

--- CP1-CursoTec.Domain/Entities/Curso.cs
+++ CP1-CursoTec.Domain/Entities/Curso.cs
@@ -14,4 +14,26 @@ public class Curso
         CargaHoraria = cargaHoraria;
         Descricao = descricao;
     }
+    
+    public  Curso(string nome, string descricao)
+    {
+        UpdateNome(nome);
+        UpdateDescricao(descricao);
+    }
+
+    public void UpdateNome(string newName)
+    {
+        if (string.IsNullOrWhiteSpace(newName))
+            throw new Exception("Nome não pode ser vazio.");
+        
+        Nome = newName;
+    }
+    
+    public void UpdateDescricao(string newDescricao)
+    {
+        if (string.IsNullOrWhiteSpace(newDescricao))
+            throw new Exception("Necessita de uma descrição.");
+        
+        Descricao = newDescricao;
+    }
 }
